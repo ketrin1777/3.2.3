d3f0d39d1cd04b7b39a158c8be6dfee3dfac85e6
nastya <nastenka29001@gmail.com>
2023-08-16 23:02:57 +0300

    Добавьте файлы проекта.

---
+++ 3.2.3/3.2.3.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <clocale>
+#include <Windows.h>
+
+struct Address
+{
+    std::string city; // Город
+    std::string street; // Улица
+    std::string house; // Номер дома (строка потому что в номере может быть буква)
+    int apartment; // Номер квартиры
+    int index; // Индекс
+};
+
+void printAddress(Address& address)
+{
+    std::cout << "Город: " << address.city << std::endl;
+    std::cout << "Улица: " << address.street << std::endl;
+    std::cout << "Номер дома: " << address.house << std::endl;
+    std::cout << "Номер квартиры: " << address.apartment << std::endl;
+    std::cout << "Индекс: " << address.index << std::endl;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Address address;
+    std::cout << "Город: ";
+    std::cin >> address.city;
+
+    std::cout << "Улица: ";
+    std::cin >> address.street;
+
+    std::cout << "Номер дома: ";
+    std::cin >> address.house;
+
+    std::cout << "Номер квартиры: ";
+    std::cin >> address.apartment;
+
+    std::cout << "Индекс: ";
+    std::cin >> address.index;
+
+    std::cout << std::endl;
+
+    printAddress(address);
+
+    return 0;
+
+}
